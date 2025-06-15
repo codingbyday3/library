66a327f2742c81c2528cd657ff3efe55538e10e2
Klemen <klemen.bussines12@gmail.com>
2025-06-15 21:45:07 +0200

    Feat: close book form button

--- library.js
+++ library.js
@@ -1,6 +1,11 @@
 const addBookBtn = document.querySelector(".add-book-btn")
 const addBookForm = document.querySelector("dialog")
+const closeBookForm = document.querySelector("dialog")
 
 addBookBtn.addEventListener("click", ()=>{
   addBookForm.showModal()
+})
+
+closeBookForm.addEventListener("click", () =>{
+  addBookForm.close()
 })
