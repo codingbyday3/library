db637b637bbba212e339639ea67bb06f62a3cf2c
Klemen <klemen.bussines12@gmail.com>
2025-06-16 10:11:02 +0200

    Feat: display books in array

--- library.js
+++ library.js
@@ -1,11 +1,62 @@
 const addBookBtn = document.querySelector(".add-book-btn")
 const addBookForm = document.querySelector("dialog")
 const closeBookForm = document.querySelector(".icon")
+const bookContainer = document.querySelector("main")
+book = {
+  id:crypto.randomUUID(),
+  title:"Harry Potter",
+  author:"J. K. Rowling",
+  year:1997,
+  comment:"Book was really good I can't wait to read it again!"
+}
 
-addBookBtn.addEventListener("click", ()=>{
-  addBookForm.showModal()
-})
+function main(){
+  displayDialog()
+  displayBook(book)
+}
 
-closeBookForm.addEventListener("click", () =>{
-  addBookForm.close()
-})
+function displayBook(bookDetails){
+  createHtml(bookDetails)
+}
+
+function createLabeledParagraph(label, value) {
+  const para = document.createElement("p");
+  const span = document.createElement("span");
+  span.className = "book-label";
+  span.textContent = label;
+  para.appendChild(span);
+  para.append(` ${value}`);
+  return para;
+}
+
+function createHtml(bookDetails) {
+  const bookDiv = document.createElement("div");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = bookDetails.title;
+  bookDiv.appendChild(h2);
+
+
+  for(bookDetail in bookDetails){
+    if (bookDetail != "id"){
+      label = bookDetail.charAt(0).toUpperCase() + bookDetail.slice(1) + ":"
+      bookDiv.appendChild(createLabeledParagraph(label, bookDetails[bookDetail]));
+    }
+  }
+
+
+  bookContainer.prepend(bookDiv);
+}
+
+function displayDialog(){
+  addBookBtn.addEventListener("click", ()=>{
+    addBookForm.showModal()
+  })
+
+  closeBookForm.addEventListener("click", () =>{
+    addBookForm.close()
+  })
+
+}
+
+main()
